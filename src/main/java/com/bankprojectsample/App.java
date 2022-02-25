b910d88ef33034314a32ec873c2f4ad1c368be24
Blessing Ajayi-Martins <blessmartins@gmail.com>
2022-02-25 09:42:51 -0500

    initial commit

---
+++ src/main/java/com/bankprojectsample/App.java
@@ -0,0 +1,13 @@
+package com.bankprojectsample;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
